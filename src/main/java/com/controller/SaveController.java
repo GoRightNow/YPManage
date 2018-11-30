4c45e440646208a81e26d192503b07aeb8102213
jacob-hua <957404156@qq.com>
2018-11-30 20:05:59 +0800

    存储

--- src/main/java/com/controller/SaveController.java
+++ src/main/java/com/controller/SaveController.java
@@ -1,10 +1,14 @@
 package com.controller;
 
+import com.entity.Area;
+import com.entity.Package;
 import com.entity.Sending;
 import com.entity.Storage;
 import com.service.SaveService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 /**
@@ -30,5 +34,15 @@ public class SaveController {
     @RequestMapping(value = "/saveSending",method = RequestMethod.POST)
     public Sending saveSending(@RequestBody Sending sending){
         return this.saveService.saveSending(sending);
+    }
+
+    @RequestMapping(value = "/findArea", method = RequestMethod.POST)
+    public List<Area> findArea(){
+        return this.saveService.findArea();
+    }
+
+    @RequestMapping(value = "/findPackage",method = RequestMethod.GET)
+    public List<Package> findPackage(@RequestParam Integer areaId){
+        return this.saveService.findPackage(areaId);
     }
 }
--- src/main/java/com/service/SaveService.java
+++ src/main/java/com/service/SaveService.java
@@ -1,7 +1,11 @@
 package com.service;
 
+import com.entity.Area;
+import com.entity.Package;
 import com.entity.Sending;
 import com.entity.Storage;
+
+import java.util.List;
 
 /**
  * Created by RedLee on 2018/11/29.
@@ -12,4 +16,8 @@ public interface SaveService {
     public Storage saveStorage(Storage storage);
 
     public Sending saveSending(Sending sending);
+
+    public List<Area> findArea();
+
+    public List<Package> findPackage(Integer areaId);
 }
